6b9c07415beffdf01a7e0d5cb25df57741eae848
16Shadows <egor.0.00@mail.ru>
2024-08-20 12:18:43 +0500

    refactor: Меню пагинации
    Уменьшено количество вызовов len в меню пагинации, где это возможно.
    Добавлен вывод общего числа записей в списке.

--- modules/menu/pagination.py
+++ modules/menu/pagination.py
@@ -37,28 +37,31 @@ class PaginationMenu[T](MenuBase):
 
     @MenuBase.text.getter
     def text(self: Self) -> str:
-        if len(self._items) < 1:
+        items_len = len(self._items)
+        if items_len < 1:
             return 'Список пуст.'
         
         if self._text_generator is None:
-            return f'Страница {self._current_page + 1}/{ self._page_count }'
+            return f'Страница {self._current_page + 1}/{ self._page_count }\nВсего записей: {items_len}'
         else:
             res = ''
-            for i in range(self._current_page * self._pageSize, min((self._current_page + 1) * self._pageSize, len(self._items))):
-                res += self._text_generator(self._items[i])
-            res += f'Страница {self._current_page + 1}/{ self._page_count }'
+            for item in self._items[self._current_page * self._pageSize:(self._current_page + 1) * self._pageSize]:
+                res += self._text_generator(item) + '\n'
+            res += f'Страница {self._current_page + 1}/{ self._page_count }\nВсего записей: {items_len}'
             return res
 
     @MenuBase.entries.getter
     def entries(self: Self) -> list[MenuEntryBase]:
         entries : list[MenuEntryBase] = []
 
-        if len(self._items) > 0:
+        items_cnt = len(self._items)
+
+        if items_cnt > 0:
             #Добавить опцию изменения размера страницы
             entries.append(StaticMenuEntry('Изменить размер страницы', self._change_page_size))
 
             #Добавить опцию перехода на следующую страницу, если не на последней странице
-            if (self._current_page + 1) * self._pageSize < len(self._items):
+            if (self._current_page + 1) * self._pageSize < items_cnt:
                 entries.append(StaticMenuEntry('Следующая страница', self._next_page))
 
             #Добавить опцию перехода на предыдущую страницу, если не на первой странице
@@ -67,8 +70,8 @@ class PaginationMenu[T](MenuBase):
 
             #Добавить все записи текущей страницы как пункты меню
             if self._entry_generator is not None:
-                for i in range(self._current_page * self._pageSize, min((self._current_page + 1) * self._pageSize, len(self._items))):
-                    entries.append(self._entry_generator(self._items[i]))
+                for item in self._items[self._current_page * self._pageSize:(self._current_page + 1) * self._pageSize]:
+                    entries.append(self._entry_generator(item))
                 
 
         #Добавить опцию перехода к предыдущему меню
@@ -76,16 +79,6 @@ class PaginationMenu[T](MenuBase):
 
         return entries
 
-    def _previous_page(self: Self, _: MenuHostBase) -> None:
-        '''Перейти на предыдущую страницу, если не на первой странице.'''
-        if (self.__currentPage > 0):
-            self.__currentPage -= 1
-
-    def _next_page(self: Self, _: MenuHostBase) -> None:
-        '''Перейти на следующую страницу, если не на последней странице'''
-        if self.__currentPage * self._pageSize < len(self._items):
-            self.__currentPage += 1
-    
     @property
     def _page_count(self: Self) -> int:
         '''Число страниц с текущими настройками'''
@@ -96,6 +89,16 @@ class PaginationMenu[T](MenuBase):
         '''Текущая страница, ограниченная числом страниц.'''
         self.__currentPage = min(self.__currentPage, self._page_count - 1)
         return self.__currentPage
+
+    def _previous_page(self: Self, _: MenuHostBase) -> None:
+        '''Перейти на предыдущую страницу, если не на первой странице.'''
+        if (self._current_page > 0):
+            self.__currentPage -= 1
+
+    def _next_page(self: Self, _: MenuHostBase) -> None:
+        '''Перейти на следующую страницу, если не на последней странице'''
+        if self._current_page < self._page_count:
+            self.__currentPage += 1
 
     def _change_page_size(self: Self, host:MenuHostBase) -> None:
         '''Изменить число записей на странице'''
